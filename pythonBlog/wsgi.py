c8f6176cc8e0c12f7dad4abab0ee6c8b008eaa9e
阮晨光 <117815667@qq.com>
2020-05-09 20:35:38 +0800

    修改项目名称引起的问题修复

--- pythonBlog/wsgi.py
+++ pythonBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for myBlog project.
+WSGI config for pythonBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myBlog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pythonBlog.settings')
 
 application = get_wsgi_application()
