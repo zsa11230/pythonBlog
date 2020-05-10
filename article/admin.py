999dd272f5accfecda2f497c18b46c82cca6fdd0
阮晨光 <117815667@qq.com>
2020-05-10 15:40:55 +0800

    首页完成部分，并消除找不到模板的警告

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article,Tags,Category
 
-# Register your models here.
+
+admin.site.register(Tags)
+admin.site.register(Article)
+admin.site.register(Category)
--- article/views.py
+++ article/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from article import models
 
 
 def index(request):
-    return render(request,'index.html')
+    blog_index = models.Article.objects.all().order_by('-id')
+    context = {
+        'articles': blog_index,  # 将数据保存在blog_index
+    }
+    return render(request, 'index.html', context)
