5fe2061f2caa90d9d924d02a01825a743d3a7da5
阮晨光 <117815667@qq.com>
2020-05-10 16:48:29 +0800

    首页标签部分完成

--- article/views.py
+++ article/views.py
@@ -4,7 +4,9 @@ from article import models
 
 def index(request):
     blog_index = models.Article.objects.all().order_by('-id')
+    tags = models.Tags.objects.order_by('-id')[0:10]
     context = {
-        'articles': blog_index,  # 将数据保存在blog_index
+        'articles': blog_index,  # 文章数据
+        'tags': tags,  # 标签
     }
     return render(request, 'index.html', context)
