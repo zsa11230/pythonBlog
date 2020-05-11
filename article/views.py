bdb0640cfac6389b3fda8ed62c551a31b546e782
阮晨光 <117815667@qq.com>
2020-05-11 16:25:24 +0800

    首页banner部分改为取文章标题图

--- article/views.py
+++ article/views.py
@@ -5,8 +5,20 @@ from article import models
 def index(request):
     blog_index = models.Article.objects.all().order_by('-id')
     tags = models.Tags.objects.order_by('-id')[0:10]
+    images = []
+    for art in blog_index:
+        if len(art.image_url) != 0:
+            if len(images) < 5:
+                img = models.Article()
+                img.image_url = art.image_url
+                img.title = art.title
+                img.id = art.id
+                images.append(img)
+            else:
+                break
     context = {
         'articles': blog_index,  # 文章数据
         'tags': tags,  # 标签
+        'images': images,  # 轮播图
     }
     return render(request, 'index.html', context)
