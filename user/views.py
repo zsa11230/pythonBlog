e0d94b26fbc133cf67cc1bf43255c3d649344630
阮晨光 <117815667@qq.com>
2020-05-19 14:56:39 +0800

    登入登出跳转问题

--- user/views.py
+++ user/views.py
@@ -3,10 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
-def register():
-    return None
-
-
+# 用户登录，若账号不存在则直接注册并登录
 def user_login(request):
     if request.method == "GET":
         return render(request, "login.html")
@@ -17,12 +14,8 @@ def user_login(request):
         if exist_flag > 0:
             user = authenticate(username=username, password=password)  # 只是验证功能，还没有登录
             if user:
-                print(user)  # username
                 login(request, user)  # 验证通过，登录
-                # 内部有request.user=user     可以用模板{{request.user}}
-                # return redirect(request.GET.get("next", '/crm'))  # http://127.0.0.1:8080/login?next=/crm
-                # 登录成功默认跳转用户信息页面，如果是其他页面来的，登录后跳转到其他页面
-                return render(request, 'login.html')
+                return redirect('/')
             else:
                 print(user)  # None
                 print(type(user))  # <class 'NoneType'>
@@ -32,13 +25,15 @@ def user_login(request):
             # 若用户名不存在则直接注册新用户
             new_user = User.objects.create_user(username=username, password=password)
             login(request, new_user)
-            return render(request, 'login.html')
+            return redirect('/')
 
 
+# 登录页面
 def login_html(request):
     return render(request, 'login.html')
 
 
+# 登出
 def user_logout(request):
     logout(request)
-    return redirect('/login')
+    return redirect('/')
